b8e1fbb7032f870c496e85d9dc569789d6ede4f6
muthu <muthu.1177@yahoo.in>
2019-06-14 12:25:19 +0530

    code modified muthu

--- sample.java
+++ sample.java
@@ -3,6 +3,8 @@ class sample
 public static void main(String args[]){
 	System.out.println("jenkins job test 1 ");
  System.out.println("jenkins branch test 1 ");
-ystem.out.println("jenkins branch test 2 ");
+ System.out.println("jenkins branch change test ");
+
+
 }
 }
